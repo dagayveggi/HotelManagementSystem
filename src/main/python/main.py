f07e70253ac4e081dd58995a4507c48156837eba
Hatism <hab.ziggie@gmail.com>
2019-05-29 04:09:49 +0200

    Added main window table update function

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,5 +1,6 @@
 from fbs_runtime.application_context import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow, QDialog
+from PyQt5 import QtCore
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from newReservation import Ui_Reservation
 from room import Ui_Room
@@ -19,6 +20,9 @@ class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
         window.setWindowTitle("HotelManagementSystem v" + version)
         #window.resize(350, 150)
         window.show()
+        db = QSqlDatabase('QSQLITE')
+        db.setDatabaseName(self.get_resource('hotel.db'))
+        model = QSqlTableModel(self.app, db)
         """conn = sqlite3.connect(self.get_resource('hotel.db'))
         c = conn.cursor()
         c.execute('SELECT * FROM Room')
@@ -26,7 +30,23 @@ class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
         ui.newRes.triggered.connect(self.newResDialog)
         ui.newRoom.triggered.connect(self.newRoomDialog)
         ui.newService.triggered.connect(self.newServiceDialog)
+        self.updateTable(ui, db, model)
         return self.app.exec_()                 # 3. End run() with this line
+
+    def updateTable(self, Ui, db, model):
+        db.open()
+        model.setTable(f"CurrentReservation")
+        model.setHeaderData(0, QtCore.Qt.Horizontal,'Reserv. ID')
+        model.setHeaderData(1, QtCore.Qt.Horizontal,'Customer ID')
+        model.setHeaderData(2, QtCore.Qt.Horizontal,'Room #')
+        model.setHeaderData(3, QtCore.Qt.Horizontal,'From')
+        model.setHeaderData(4, QtCore.Qt.Horizontal,'To')
+        model.setHeaderData(5, QtCore.Qt.Horizontal,'Discount')
+        model.setHeaderData(6, QtCore.Qt.Horizontal,'Extension')
+        model.setHeaderData(7, QtCore.Qt.Horizontal,'Net Total')
+        model.select()
+        Ui.tableView.setModel(model)
+        db.close()
     
     def newResDialog(self):
         ui = Ui_Reservation()
